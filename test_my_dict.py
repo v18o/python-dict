6c1ee122b2392dc67ce6c4a540146ce1234b141e
Vika Romanchenko <v18o@Vikas-MacBook-Air.local>
2021-05-29 19:39:02 +0300

    __eq__, __ne__ tests

--- test_my_dict.py
+++ test_my_dict.py
@@ -9,6 +9,7 @@ from my_dict import MyDict
 
 
 def _initialize_dicts():
+    my_dict, python_dict = MyDict(5), {}
     keys_values = [
         ('key1', 1),
         ('key2', 2),
@@ -19,11 +20,8 @@ def _initialize_dicts():
         ('key5', 7),  # same key as previous
         ('key6', 8),  # more values than space in MyDict, collision guarantee
     ]
-    my_dict = MyDict(5)
-    python_dict = {}
     for key, value in keys_values:
-        my_dict[key] = value
-        python_dict[key] = value
+        my_dict[key], python_dict[key] = value, value
 
     return my_dict, python_dict
 
@@ -41,3 +39,23 @@ def test_values_method():
 def test_items_method():
     my_dict, python_dict = _initialize_dicts()
     assert sorted(my_dict.items()) == sorted(python_dict.items())
+
+
+def test_eq_method():
+    my_dict1, my_dict2 = MyDict(5), MyDict(10)
+    keys_values = [
+        ('key1', 1),
+        ('key2', 2),
+        ('key3', 3),
+    ]
+    for key, value in keys_values:
+        my_dict1[key] = value
+        my_dict2[key] = value
+
+    assert my_dict1 == my_dict2
+
+
+def test_ne_method():
+    my_dict1, my_dict2 = MyDict(5), MyDict(5)
+    my_dict2[1], my_dict2[1] = False, True
+    assert my_dict1 != my_dict2
